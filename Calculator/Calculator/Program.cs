b4258e4daee9d3f43b669384f26ce45518ece564
Muallimaqiz2003 <robiyahakimova2003@gmail.com>
2024-03-19 05:44:35 +0500

    WIP

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -4,7 +4,44 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            while (true)
+            {
+                Console.WriteLine("AMalni kiriting : ");
+                string amal = Console.ReadLine();      
+                if (amal.Contains("+"))
+                {
+                    string[] sonlar = amal.Split('+');
+                    Console.WriteLine(int.Parse(sonlar[0]) + int.Parse(sonlar[1]));
+
+                }
+                else if (amal.Contains("-"))
+                {
+                    string[] sonlar = amal.Split('-');
+                    Console.WriteLine(int.Parse(sonlar[0]) - int.Parse(sonlar[1]));
+                }
+                else if (amal.Contains("*"))
+                {
+                    string[] sonlar = amal.Split('*');
+                    Console.WriteLine(int.Parse(sonlar[0]) * int.Parse(sonlar[1]));
+                }
+                else if (amal.Contains("/"))
+                {
+                    string[] sonlar = amal.Split('/');
+                    Console.WriteLine(int.Parse(sonlar[0]) / int.Parse(sonlar[1]));
+                }
+                else if (!amal.Contains("%"))
+                {
+                    string[] sonlar = amal.Split('%');
+                    Console.WriteLine(int.Parse(sonlar[0]) % int.Parse(sonlar[1]));
+                }
+                else if (amal.Contains("^"))
+                {
+                    string[] sonlar = amal.Split('^');
+                    Console.WriteLine(int.Parse(sonlar[0]) ^ int.Parse(sonlar[1]));
+                }
+
+               
+            }
         }
     }
 }
