5c9e8ecb9140645ccbe5323adbf23702e06ac5f8
Muallimaqiz2003 <robiyahakimova2003@gmail.com>
2024-03-19 05:44:59 +0500

    End

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -39,8 +39,18 @@
                     string[] sonlar = amal.Split('^');
                     Console.WriteLine(int.Parse(sonlar[0]) ^ int.Parse(sonlar[1]));
                 }
+                Console.WriteLine("<- TUGATISH     DAVOM ETISH->");
+                var key = Console.ReadKey();
+                if (key.Key == ConsoleKey.RightArrow)
+                {
+                    Console.Clear();
+                    continue;
+                }
+                else if (key.Key == ConsoleKey.LeftArrow)
+                {
+                    break;
+                }
 
-               
             }
         }
     }
